cd8201a2a2508abcaaeeb916572f5df8bd0fbbe0
iFeng Plus <feng0175@e.ntu.edu.sg>
2024-11-18 12:22:22 +0000

    brucesohandsome_2.5

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask
 from flask import render_template, request
 
 app = Flask("__name__")
+
 @app.route("/", methods = ["GET","POST"])
 def index():
-    return(render_template("index.html"))
+    return(render_template("index2.html"))
+
+@app.route("/main", methods = ["GET","POST"])
+def main():
+    name = request.form.get('q')
+    return(render_template("main.html"))
 
 if __name__ == "__main__":
     app.run(debug= True)
